1ee56e19bc0b0aeaff0e8ea7a573f11cef026982
Ameek Singh <am33ksingh@gmail.com>
2024-02-17 13:01:51 -0800

    More initial updates

---
+++ extension/script.js
@@ -0,0 +1,61 @@
+const apiEndpoint2 = '';
+
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    // tabs is an array of tab objects
+    var currentTab = tabs[0];
+    
+    // Access the URL of the current tab
+    var currentTabUrl = currentTab.url;
+    
+    // Now you can use currentTabUrl as needed
+    document.getElementById("urlInput").value = currentTabUrl;
+  
+  
+});
+  
+document.getElementById("openButton").addEventListener("click", function() {
+    openUrl();
+});
+  
+
+function openUrl() {
+    var url = document.getElementById("urlInput").value;
+    if (url) {
+        sendUrlEmbeded(url);  // api call to send url
+        saveUrl(url);
+    }
+}
+
+// test if url is saved
+function saveUrl(url) {
+    console.log("URL saved:", url);
+}
+
+// send embedded url
+function sendUrlEmbedded(url) {
+    const fullApiUrl = `${apiEndpoint2}`;
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+    const requestBody = JSON.stringify({ url: url });
+    fetch(fullApiUrl, {
+      method: 'POST',
+      headers: headers,
+      body: requestBody,
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("sucess for 2", data);
+        //createAnswer(data.)
+        
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  }
+  
--- flask_framework.py
+++ flask_framework.py
@@ -1,12 +1,21 @@
-import flask
+from flask import Flask, request, jsonify
 import epilepsyTracker
-from flask_cors import COR
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={"/": {"origins": "*"}})
 
 
-@app.route('/')
+@app.route('/epilepsy_check', methods= ['GET'])
 def epilepsy_check():
+    try:
+        data = response.json
+        URL = data.get('url')
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': str(e)}), 500
+    
+
+
 
 
