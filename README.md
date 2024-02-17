26dd97102b4fe0f503c8e438fb12c80860b737fa
Ameek Singh <am33ksingh@gmail.com>
2024-02-17 12:48:24 -0800

    Initial updates to all files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Hack4Humanity2024
+
+Our Chrome Extension fulfills a necessary niche - epileptic seizures.
--- flask_framework.py
+++ flask_framework.py
@@ -1,7 +1,12 @@
 import flask
 import epilepsyTracker
+from flask_cors import COR
+
+app = Flask(__name__)
+CORS(app, resources={"/": {"origins": "*"}})
 
 
-
+@app.route('/')
+def epilepsy_check():
 
 
