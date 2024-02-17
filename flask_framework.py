4723b7cc06ace3e50582f86f85e0d55e86108169
Ameek Singh <am33ksingh@gmail.com>
2024-02-17 13:11:39 -0800

    main.css and flas_framework.py

--- flask_framework.py
+++ flask_framework.py
@@ -10,11 +10,16 @@ CORS(app, resources={"/": {"origins": "*"}})
 def epilepsy_check():
     try:
         data = response.json
-        URL = data.get('url')
+        yt_url = data.get('url')
     except Exception as e:
         print(str(e))
         return jsonify({'error': str(e)}), 500
+    return jsonify({'timestamps': yt_url})
+
     
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5003)
+
 
 
 
