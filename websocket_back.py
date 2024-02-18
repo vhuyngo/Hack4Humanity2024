82085a59a9bf3b00b0221cb44fdf6d511a0bf021
Ameek Singh <am33ksingh@gmail.com>
2024-02-17 16:39:27 -0800

    removed old run

--- websocket_back.py
+++ websocket_back.py
@@ -15,5 +15,5 @@ async def handler(websocket, path):
 
 start_server = websockets.serve(handler, "localhost", 6789)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+asyncio.get_event_loop().run(start_server)
+# asyncio.get_event_loop().run_forever()
