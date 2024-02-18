2cec318070206fc3faa3c4706c0a5d916b8a9d27
Ameek Singh <am33ksingh@gmail.com>
2024-02-17 17:37:18 -0800

    more websocket

--- extension/script.js
+++ extension/script.js
@@ -1,5 +1,5 @@
 const apiEndpoint2 = 'http://172.20.209.75:5003/epilepsy_check';
-const webEndpoint = 'ws://192.168.64.1:6789/';
+const webEndpoint = 'ws://172.20.215.140:6789/';
 
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     // tabs is an array of tab objects
