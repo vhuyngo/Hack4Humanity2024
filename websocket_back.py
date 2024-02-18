2eef8564e72438c572ad8bfe51ce583ab457237a
Ameek Singh <am33ksingh@gmail.com>
2024-02-17 17:13:29 -0800

    changed ip

--- websocket_back.py
+++ websocket_back.py
@@ -8,7 +8,9 @@ async def echo(websocket, path):
         response = json.dumps({"status": "Received"})
         await websocket.send(response)
 
-start_server = websockets.serve(echo, "localhost", 6789)
+# client_ip = websockets.getspeername()
+
+start_server = websockets.serve(echo, "192.168.64.1", 6789)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
